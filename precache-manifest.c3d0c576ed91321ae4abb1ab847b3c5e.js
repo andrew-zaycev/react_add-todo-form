46c8eae2a5de75428df43f88a7236c583cd0db64
RioDron <riopositivniy@gmail.com>
2021-01-24 21:41:51 +0200

    Updates

---
+++ precache-manifest.c3d0c576ed91321ae4abb1ab847b3c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5188166620c99078dda445940d821ce7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0542522a2f9224f25d00",
+    "url": "./static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "c661c591488051d94834",
+    "url": "./static/js/2.53b1748d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.53b1748d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0542522a2f9224f25d00",
+    "url": "./static/js/main.a74b2852.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
